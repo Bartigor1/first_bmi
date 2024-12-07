b6a2ba95525cf6eb081a081f460349a0437854ed
Bartigor1 <lifeexcel2016@gmail.com>
2024-12-07 17:09:22 +0200

    1

---
+++ app/index.tsx
@@ -0,0 +1,32 @@
+import { View, StyleSheet, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+  <View style={{ flex: 1, backgroundColor: '#6C63FF' }}>
+    <Text style={{ 
+      // fontStyle: 'normal', 
+      fontWeight: '700', 
+      // fontSize: 25,
+      }}>
+      Get Started with
+    Tracking Your Health!</Text>
+
+
+  </View>;
+  )
+}
+/* Get Started with Tracking Your Health! */
+
+// position: absolute;
+// width: 270px;
+// height: 65px;
+// left: 55px;
+// top: 517px;
+
+// font-weight: 700;
+// font-size: 25px;
+// line-height: 33px;
+// /* or 132% */
+
+// color: #FFFFFF;
+
