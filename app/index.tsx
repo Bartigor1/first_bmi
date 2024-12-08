8a743c8d58ed9fc76806fcd2d758f7aa2b9276f5
Bartigor1 <lifeexcel2016@gmail.com>
2024-12-08 13:35:29 +0200

    1

--- app/index.tsx
+++ app/index.tsx
@@ -7,62 +7,59 @@ export default function HomeScreen() {
   };
 
   return (
-  <View style={styles.container}>
-    <Text style={styles.getStartedText}>
-      Get Started with Tracking Your Health!
-    </Text>
-    <Text style={styles.noteText}>
-    Calculate your BMI and stay on top of your wellness journey, effortlessly.
-    </Text>
-   <View style={{width: '100%' }}>
-    <TouchableOpacity  
-    onPress={handlePressButton} 
-    style={styles.button}>
+    <View style={styles.container}>
+      <Text style={styles.getStartedText}>
+        Get Started with Tracking Your Health!
+      </Text>
+      <Text style={styles.noteText}>
+        Calculate your BMI and stay on top of your wellness journey, effortlessly.
+      </Text>
+      <View style={{ width: '100%' }}>
+        <TouchableOpacity
+          onPress={handlePressButton}
+          style={styles.button}>
           <Text style={styles.buttonText}>Get Started</Text>
         </TouchableOpacity>
-   </View>
-  </View>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-container: {
-    flex: 1, 
-  backgroundColor: '#6C63FF',
-  justifyContent: 'center',
-  alignItems: 'center',
-},
-getStartedText: {
-    fontWeight: '700', 
-  fontSize: 25,
-  lineHeight: 33,
-  color: '#FFFFFF',
-  marginLeft: 55,
-  marginRight: 68,
-},
-noteText: {
-  fontSize: 15.33, 
-  lineHeight: 19,
-  color: '#C6C3F9',
-  marginHorizontal: 56,
-  marginTop: 21,
-},
-button: {
-  alignItems: 'center', 
-  justifyContent: 'center',
-  backgroundColor: '#ffffff', 
-  marginHorizontal: 30,
-  marginTop: 38,
-  paddingVertical: 27,
-  borderRadius: 63,
+  container: {
+    flex: 1,
+    backgroundColor: '#6C63FF',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  getStartedText: {
+    fontWeight: '700',
+    fontSize: 25,
+    lineHeight: 33,
+    color: '#FFFFFF',
+    marginLeft: 55,
+    marginRight: 68,
+  },
+  noteText: {
+    fontSize: 15.33,
+    lineHeight: 19,
+    color: '#C6C3F9',
+    marginHorizontal: 56,
+    marginTop: 21,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ffffff',
+    marginHorizontal: 30,
+    marginTop: 38,
+    paddingVertical: 27,
+    borderRadius: 63,
   },
   buttonText: {
-    fontSize: 17.6, 
+    fontSize: 17.6,
     fontWeight: 500,
     lineHeight: 21,
     color: '#081854',
   }
 });
-
-
-
